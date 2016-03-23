87e997fa9d649989fedda16c90677b413e5eca85
abessems <choum20@gmail.com>
2016-03-23 11:54:01 +0100

    Ajout des mouvements dans toutes les directions

--- src/actors/stricty-boy.js
+++ src/actors/stricty-boy.js
@@ -1,18 +1,27 @@
 var StrictyBoy = (function() {
     
+    var p = createjs.extend(StrictyBoy, createjs.Container);
+    
     var defaultSize = {
         width: 20,
         height: 30
     };
         
     var velocity = 10;
+    var goUp;
+    var goDown;
+    var goRight;
+    var goLeft;
     
+    // Constructor
     function StrictyBoy(stageSize, size) {
         this.shape = new createjs.Shape();
         this.size = size || { width: defaultSize.width, height: defaultSize.height };
         this._stageSize = stageSize;
+        this.goUp = this.goDown = this.goRight = this.goLeft = false;
     }
     
+    // Initialisation
     StrictyBoy.prototype.init = function() {
         this.shape.graphics
             .beginFill("DeepSkyBlue")
@@ -22,32 +31,46 @@ var StrictyBoy = (function() {
         this.shape.y =  this._stageSize.height - this.size.height - 10;    
     } 
     
-    StrictyBoy.prototype.moveRight = function() {
-        if (this.shape.x + this.size.width < this._stageSize.width)
-            this.shape.x += velocity;
-        else 
-            this.shape.x = this._stageSize.width - this.size.width;
-    }
+    // Starting to move on a direction
+    StrictyBoy.prototype.moveRight = function() { goRight = true; }    
+    StrictyBoy.prototype.moveLeft = function() { goLeft = true; }    
+    StrictyBoy.prototype.moveUp = function() { goUp = true; }    
+    StrictyBoy.prototype.moveDown = function() { goDown = true; }
     
-    StrictyBoy.prototype.moveLeft = function() {
-        if (this.shape.x > velocity)
-            this.shape.x -= velocity;
-        else
-            this.shape.x = 0; 
-    }
+    // Stopping to move on a direction
+    StrictyBoy.prototype.moveRightStop = function() { goRight = false; }    
+    StrictyBoy.prototype.moveLeftStop = function() { goLeft = false; }    
+    StrictyBoy.prototype.moveUpStop = function() { goUp = false; }    
+    StrictyBoy.prototype.moveDownStop = function() { goDown = false; }
     
-    StrictyBoy.prototype.moveUp = function() {
-        if (this.shape.y > velocity)
-            this.shape.y -= velocity;
-        else
-            this.shape.y = 0; 
-    }
-    
-    StrictyBoy.prototype.moveDown = function() {
-        if (this.shape.y + this.size.height < this._stageSize.height)
-            this.shape.y += velocity;
-        else 
-            this.shape.y = this._stageSize.height - this.size.height;
+    // Event fires on each tick
+    p.tickEvent = function tickEvent(event){
+        
+        // Vertical movement
+        if(goUp){
+            if (this.shape.y > velocity)
+                this.shape.y -= velocity;
+            else
+                this.shape.y = 0; 
+        }else if(goDown){
+            if (this.shape.y + this.size.height < this._stageSize.height)
+                this.shape.y += velocity;
+            else 
+                this.shape.y = this._stageSize.height - this.size.height;
+        }
+        
+        // Horizontal movement
+        if(goRight){
+            if (this.shape.x + this.size.width < this._stageSize.width)
+                this.shape.x += velocity;
+            else 
+                this.shape.x = this._stageSize.width - this.size.width;
+        }else if(goLeft){
+            if (this.shape.x > velocity)
+                this.shape.x -= velocity;
+            else
+                this.shape.x = 0; 
+        }       
     }
     
     return StrictyBoy;
--- src/app.js
+++ src/app.js
@@ -1,3 +1,15 @@
+
+var KEYCODE_ENTER = 13;		//useful keycode
+var KEYCODE_SPACE = 32;		//useful keycode
+var KEYCODE_UP = 38;		//useful keycode
+var KEYCODE_LEFT = 37;		//useful keycode
+var KEYCODE_RIGHT = 39;		//useful keycode
+var KEYCODE_DOWN = 40;		//useful keycode
+var KEYCODE_W = 87;			//useful keycode
+var KEYCODE_A = 65;			//useful keycode
+var KEYCODE_D = 68;			//useful keycode
+
+
 var stage;
 var stageSize = {
     width: 500,
@@ -8,7 +20,8 @@ var strictyBoy;
 var actionsKeyMap;
 
 function init() {
-    document.onkeydown = handleKeys;
+    document.onkeydown = handleKeysDown;
+    document.onkeyup = handleKeysUp;
     
     stage = new createjs.Stage("demoCanvas");
     stage.canvas.width = stageSize.width;
@@ -18,37 +31,82 @@ function init() {
     strictyBoy.init();                
                     
     stage.addChild(strictyBoy.shape);
-    
+    stage.update();
     initActionsKeyMap();
     
-    createjs.Ticker.setFPS(60);
-    createjs.Ticker.addEventListener("tick", stage);
+    //createjs.Ticker.setFPS(60);
+    //createjs.Ticker.addEventListener("tick", stage);
+    //start game timer
+	if (!createjs.Ticker.hasEventListener("tick")) {
+		createjs.Ticker.addEventListener("tick", tickEvent);
+	}
+}
+
+function tickEvent(event){
+    strictyBoy.tickEvent(event);
+    stage.update(event);
 }
 
 // Note: moved initialisation here to be sure that actors are instantiated.
 function initActionsKeyMap() {
-    actionsKeyMap = {
-        37: {
+    actionsKeyDownMap = {
+        37: { //KEYCODE_LEFT
             actor: strictyBoy,
             action: strictyBoy.moveLeft
         },
-        39: {
-            actor: strictyBoy,
-            action: strictyBoy.moveRight
-        },
-        38: {
+        38: { //KEYCODE_UP
             actor: strictyBoy,
             action: strictyBoy.moveUp
         },
-        40: {
+        39: { //KEYCODE_RIGHT
+            actor: strictyBoy,
+            action: strictyBoy.moveRight
+        },
+        40: { //KEYCODE_DOWN
             actor: strictyBoy,
             action: strictyBoy.moveDown
         }
     }
+    actionsKeyUpMap = {
+        37: { //KEYCODE_LEFT
+            actor: strictyBoy,
+            action: strictyBoy.moveLeftStop
+        },
+        38: { //KEYCODE_UP
+            actor: strictyBoy,
+            action: strictyBoy.moveUpStop
+        },
+        39: { //KEYCODE_RIGHT
+            actor: strictyBoy,
+            action: strictyBoy.moveRightStop
+        },
+        40: { //KEYCODE_DOWN
+            actor: strictyBoy,
+            action: strictyBoy.moveDownStop
+        }
+    }
 }
 
-function handleKeys(event) {
-    var actionKeyMap = actionsKeyMap[event.keyCode];
+function handleKeysDown(event) {
+    //cross browser issues exist
+	if (!event) {
+		var event = window.event;
+	}
+    
+    var actionKeyMap = actionsKeyDownMap[event.keyCode];
+    if (!actionKeyMap)
+        return;
+        
+    actionKeyMap.action.call(actionKeyMap.actor);
+}
+
+function handleKeysUp(event) {
+    //cross browser issues exist
+	if (!event) {
+		var event = window.event;
+	}
+    
+    var actionKeyMap = actionsKeyUpMap[event.keyCode];
     if (!actionKeyMap)
         return;
         
